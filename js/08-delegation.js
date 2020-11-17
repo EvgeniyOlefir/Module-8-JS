2cfb89521c0fdefd788304ed04db61003fa2c570
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2020-11-17 20:54:41 +0200

    Добавляет файл занятия 8 модуля

---
+++ js/08-delegation.js
@@ -0,0 +1,22 @@
+const menu = document.querySelector(".js-nav");
+
+const handleMenuClick = (e) => {
+  const current = e.target;
+  const prev = document.querySelector(".active");
+
+  if (e.currentTarget === e.target) {
+    return;
+  }
+
+  // if (e.target.nodeName !== 'A') {
+  //   return;
+  // }
+
+  if (prev) {
+    prev.classList.remove("active");
+  }
+
+  current.classList.add("active");
+};
+
+menu.addEventListener("click", handleMenuClick);
